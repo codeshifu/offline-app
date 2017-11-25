c35bd674c152619a824bc1a1f632ba6c1d550cb6
Olushi Luqman O <olushilukmon03@gmail.com>
2017-11-25 15:20:42 +0100

    serviceWorker yo

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,6 @@
 importScripts('/js/polyfills/cache-polyfill.js');
 
 // remove comment below
-/*
 let cacheName = 'gdg-umuahia';
 let urlsToCache = [
     '/',
@@ -19,9 +18,22 @@ let urlsToCache = [
     "https://scontent.flos6-1.fna.fbcdn.net/v/t1.0-9/20525306_1926624817626299_7052616052694016757_n.jpg?_nc_eui2=v1%3AAeFcY5BpwBAoVrETTLD-UjTGGNpS6MOksDvm6SdixonTHrOkef2OlP15wf5stXeCzpwr4wiEo8SRQ1wnI6oFO7TUO7e_V6QPA3OJSEGTNt02FA&oh=9e90632e9854e7a29bc89390d0c16085&oe=5AD27F95",
     "/img/3.jpg",
     "/img/4.jpg",
-]*/
+]
 
 
 // TODO: 3 cache site dependencies/assets
-
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    )
+})
 // TODO: 2 intercept request
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    )
+})
